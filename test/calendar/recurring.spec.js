bf59d00569b4064909c3f39daae0fb4d74e5a55c
mmso <mmso@mmso.se>
2019-11-12 16:05:49 +0100

    Improve recurring caching between

--- test/calendar/recurring.spec.js
+++ test/calendar/recurring.spec.js
@@ -1,9 +1,16 @@
-import { getOccurencesBetween, getOccurencesUntil } from '../../lib/calendar/recurring';
+import { getOccurencesBetween } from '../../lib/calendar/recurring';
 
 describe('recurring', () => {
     const component = {
         dtstart: {
-            value: { year: 2019, month: 3, day: 30, hours: 2, minutes: 30, seconds: 0, isUTC: false },
+            value: { year: 2019, month: 1, day: 30, hours: 2, minutes: 30, seconds: 0, isUTC: false },
+            parameters: {
+                type: 'date-time',
+                tzid: 'Europe/Zurich'
+            }
+        },
+        dtend: {
+            value: { year: 2019, month: 1, day: 30, hours: 3, minutes: 30, seconds: 0, isUTC: false },
             parameters: {
                 type: 'date-time',
                 tzid: 'Europe/Zurich'
@@ -11,39 +18,43 @@ describe('recurring', () => {
         },
         rrule: {
             value: {
-                freq: 'DAILY',
-                count: 10
+                freq: 'DAILY'
             }
         }
     };
 
-    it('should not get occurrences if it is out of range', () => {
-        const result = getOccurencesUntil(component, new Date(Date.UTC(2018, 3, 1)));
-        expect(result).toBeUndefined();
+    it('should not get occurrences between if it is out of range', () => {
+        const result = getOccurencesBetween(component, Date.UTC(2018, 3, 1), Date.UTC(2018, 3, 2));
+        expect(result).toEqual([]);
     });
 
-    it('should get occurrences until', () => {
-        const result = getOccurencesUntil(component, new Date(Date.UTC(2019, 3, 1, 3)));
+    it('should get occurrences between a range', () => {
+        const result = getOccurencesBetween(component, Date.UTC(2019, 2, 1), Date.UTC(2019, 2, 3));
 
-        expect(result.map((start) => new Date(start).toISOString())).toEqual([
-            '2019-03-30T02:30:00.000Z',
-            '2019-03-31T02:30:00.000Z',
-            '2019-04-01T02:30:00.000Z',
-            '2019-04-02T02:30:00.000Z'
+        expect(
+            result.map(([start, end]) => `${new Date(start).toISOString()} - ${new Date(end).toISOString()}`)
+        ).toEqual([
+            '2019-03-01T01:30:00.000Z - 2019-03-01T02:30:00.000Z',
+            '2019-03-02T01:30:00.000Z - 2019-03-02T02:30:00.000Z'
         ]);
     });
 
-    it('should get occurrences between', () => {
-        const start = new Date(Date.UTC(2019, 3, 1, 0));
-        const end = new Date(Date.UTC(2019, 3, 2, 0));
-        const occurrences = getOccurencesUntil(component, end);
+    it('should get cached occurrences between a range', () => {
+        const cache = {};
+        const result1 = getOccurencesBetween(component, Date.UTC(2019, 2, 1), Date.UTC(2019, 2, 3), cache);
+        const result2 = getOccurencesBetween(component, Date.UTC(2019, 2, 1), Date.UTC(2019, 2, 3), cache);
+        expect(result1).toEqual(result2);
+    });
 
+    it('should fill cache if out of range', () => {
+        const cache = {};
+        const result1 = getOccurencesBetween(component, Date.UTC(2019, 2, 1), Date.UTC(2019, 2, 3), cache);
+        const result2 = getOccurencesBetween(component, Date.UTC(2031, 2, 1), Date.UTC(2031, 2, 3), cache);
         expect(
-            getOccurencesBetween(occurrences, 30 * 60000, start, end).map((start) => new Date(start).toISOString())
-        ).toEqual(['2019-04-01T02:30:00.000Z']);
-
-        expect(
-            getOccurencesBetween(occurrences, 1440 * 60000, start, end).map((start) => new Date(start).toISOString())
-        ).toEqual(['2019-03-31T02:30:00.000Z', '2019-04-01T02:30:00.000Z']);
+            result2.map(([start, end]) => `${new Date(start).toISOString()} - ${new Date(end).toISOString()}`)
+        ).toEqual([
+            '2031-03-01T01:30:00.000Z - 2031-03-01T02:30:00.000Z',
+            '2031-03-02T01:30:00.000Z - 2031-03-02T02:30:00.000Z'
+        ]);
     });
 });
