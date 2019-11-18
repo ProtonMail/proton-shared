14028cf2ac2cc7b64eac70a2e7149f490b58877c
vincaslt <vincaslt@gmail.com>
2019-11-18 14:17:45 +0200

    Drive key initialization

--- lib/keys/driveKeys.ts
+++ lib/keys/driveKeys.ts
@@ -1,5 +1,21 @@
-import { decryptMessage, getMessage } from 'pmcrypto/lib/pmcrypto';
+import { decryptMessage, getMessage, encryptMessage, generateKey } from 'pmcrypto/lib/pmcrypto';
 import { key } from 'openpgp';
+import { ENCRYPTION_CONFIGS, ENCRYPTION_TYPES } from '../constants';
+import { generatePassphrase } from './calendarKeys';
+
+interface UnsignedEncryptionPayload {
+    message: string;
+    privateKey: key.Key;
+}
+
+export const encryptUnsigned = async ({ message, privateKey }: UnsignedEncryptionPayload) => {
+    const { data: encryptedToken } = await encryptMessage({
+        data: message,
+        privateKeys: privateKey,
+        publicKeys: privateKey.toPublic()
+    });
+    return encryptedToken as string;
+};
 
 interface UnsignedDecryptionPayload {
     armoredMessage: string;
@@ -17,4 +33,51 @@ export const decryptUnsigned = async ({ armoredMessage, privateKey }: UnsignedDe
     });
 
     return decryptedMessage as string;
+};
+
+export const generateDriveKey = async (rawPassphrase: string) => {
+    const encryptionConfigs = ENCRYPTION_CONFIGS[ENCRYPTION_TYPES.X25519];
+    const { key: privateKey, privateKeyArmored } = await generateKey({
+        userIds: [{ name: 'Drive key' }],
+        passphrase: rawPassphrase,
+        ...encryptionConfigs
+    });
+
+    await privateKey.decrypt(rawPassphrase);
+
+    return { privateKey, privateKeyArmored };
+};
+
+export const generateInitialDriveKeys = async (addressPrivateKey: key.Key) => {
+    const rawSharePassphrase = generatePassphrase();
+    const rawFolderPassphrase = generatePassphrase();
+
+    const [
+        { privateKey: sharePrivateKey, privateKeyArmored: ShareKey },
+        { privateKey: folderPrivateKey, privateKeyArmored: FolderKey },
+        SharePassphrase
+    ] = await Promise.all([
+        generateDriveKey(rawSharePassphrase),
+        generateDriveKey(rawFolderPassphrase),
+        encryptUnsigned({ message: rawSharePassphrase, privateKey: addressPrivateKey })
+    ]);
+
+    const [FolderPassphrase, FolderName] = await Promise.all([
+        await encryptUnsigned({
+            message: rawFolderPassphrase,
+            privateKey: sharePrivateKey
+        }),
+        await encryptUnsigned({
+            message: 'root',
+            privateKey: folderPrivateKey
+        })
+    ]);
+
+    return {
+        SharePassphrase,
+        FolderPassphrase,
+        ShareKey,
+        FolderKey,
+        FolderName
+    };
 };
