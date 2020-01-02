38552a0c5eae10f4848d0824de64ba77d03eefa3
vincaslt <vincaslt@gmail.com>
2020-01-02 12:32:27 +0200

    pmcrypto typing stub

--- typings/index.d.ts
+++ typings/index.d.ts
@@ -2,3 +2,12 @@ declare module 'pmcrypto/lib/openpgp' {
     const openpgp: any;
     export { openpgp };
 }
+
+declare module 'pmcrypto' {
+    const decryptMessage: any;
+    const getMessage: any;
+    const encryptMessage: any;
+    const generateKey: any;
+    const decryptPrivateKey: any;
+    export { decryptMessage, getMessage, encryptMessage, generateKey, decryptPrivateKey };
+}
