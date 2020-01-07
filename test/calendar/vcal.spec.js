a7b76a15ef1c448eec58d82226eeb9fc1c92dbb3
econdepe <econdepe@protonmail.com>
2020-01-07 18:52:42 +0100

    change one test

--- test/calendar/vcal.spec.js
+++ test/calendar/vcal.spec.js
@@ -256,7 +256,7 @@ describe('calendar', () => {
     it('should convert trigger strings into milliseconds', () => {
         expect(getMillisecondsFromTriggerString('-PT30M')).toEqual(-30 * MINUTE);
         expect(getMillisecondsFromTriggerString('PT1H')).toEqual(HOUR);
-        expect(getMillisecondsFromTriggerString('P1D')).toEqual(DAY);
+        expect(getMillisecondsFromTriggerString('-P1D')).toEqual(-DAY);
         expect(getMillisecondsFromTriggerString('-PT2H34M12S')).toEqual(-2 * HOUR - 34 * MINUTE - 12 * SECOND);
         expect(getMillisecondsFromTriggerString('P2W1DT1S')).toEqual(2 * WEEK + DAY + SECOND);
     });
