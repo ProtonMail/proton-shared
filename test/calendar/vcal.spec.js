08f818e5950034a5c2762b6a590d7e3f563d7b9f
econdepe <econdepe@protonmail.com>
2020-01-29 18:48:42 +0100

    tests for daily

--- test/calendar/vcal.spec.js
+++ test/calendar/vcal.spec.js
@@ -46,6 +46,36 @@ TRIGGER:-PT15H
 END:VALARM
 END:VEVENT`;
 
+const veventRruleDaily1 = `BEGIN:VEVENT
+UID:7E018059-2165-4170-B32F-6936E88E61E5
+DTSTART:20190719T120000Z
+DTEND:20190719T130000Z
+RRULE:FREQ=DAILY;COUNT=10;INTERVAL=3
+END:VEVENT`;
+
+const veventRruleDaily2 = `BEGIN:VEVENT
+UID:7E018059-2165-4170-B32F-6936E88E61E5
+DTSTART;VALUE=DATE:20190719
+DTEND;VALUE=DATE:20190719
+RRULE:FREQ=DAILY;UNTIL=20200130
+END:VEVENT`;
+
+const veventRruleDaily3 = `BEGIN:VEVENT
+UID:7E018059-2165-4170-B32F-6936E88E61E5
+DTSTART:20190719T120000Z
+DTEND:20190719T130000Z
+RRULE:FREQ=DAILY;UNTIL=20200130T225959Z
+END:VEVENT`;
+
+const veventRruleDaily4 = `BEGIN:VEVENT
+UID:7E018059-2165-4170-B32F-6936E88E61E5
+DTSTART;TZID=America/New_York:20190719T120000
+DTEND:20190719T130000Z
+RRULE:FREQ=DAILY;UNTIL=20200130T225959Z
+END:VEVENT`;
+
+const veventsRruleDaily = [veventRruleDaily1, veventRruleDaily2, veventRruleDaily3, veventRruleDaily4];
+
 const veventRruleWeekly1 = `BEGIN:VEVENT
 UID:7E018059-2165-4170-B32F-6936E88E61E5
 DTSTART:20190719T120000Z
@@ -186,8 +216,92 @@ describe('calendar', () => {
         });
     });
 
-    it('should parse rrule in vevent', () => {
-        const components = [...veventsRruleWeekly, ...veventsRruleMonthly, ...veventsRruleYearly].map(parse);
+    it('should parse daily rrule in vevent', () => {
+        const components = veventsRruleDaily.map(parse);
+
+        expect(components).toEqual([
+            {
+                component: 'vevent',
+                uid: {
+                    value: '7E018059-2165-4170-B32F-6936E88E61E5'
+                },
+                dtstart: {
+                    value: { year: 2019, month: 7, day: 19, hours: 12, minutes: 0, seconds: 0, isUTC: true }
+                },
+                dtend: {
+                    value: { year: 2019, month: 7, day: 19, hours: 13, minutes: 0, seconds: 0, isUTC: true }
+                },
+                rrule: {
+                    value: {
+                        freq: 'DAILY',
+                        count: 10,
+                        interval: 3,
+                        until: undefined
+                    }
+                }
+            },
+            {
+                component: 'vevent',
+                uid: {
+                    value: '7E018059-2165-4170-B32F-6936E88E61E5'
+                },
+                dtstart: {
+                    value: { year: 2019, month: 7, day: 19 },
+                    parameters: { type: 'date' }
+                },
+                dtend: {
+                    value: { year: 2019, month: 7, day: 19 },
+                    parameters: { type: 'date' }
+                },
+                rrule: {
+                    value: {
+                        freq: 'DAILY',
+                        until: { year: 2020, month: 1, day: 30 }
+                    }
+                }
+            },
+            {
+                component: 'vevent',
+                uid: {
+                    value: '7E018059-2165-4170-B32F-6936E88E61E5'
+                },
+                dtstart: {
+                    value: { year: 2019, month: 7, day: 19, hours: 12, minutes: 0, seconds: 0, isUTC: true }
+                },
+                dtend: {
+                    value: { year: 2019, month: 7, day: 19, hours: 13, minutes: 0, seconds: 0, isUTC: true }
+                },
+                rrule: {
+                    value: {
+                        freq: 'DAILY',
+                        until: { year: 2020, month: 1, day: 30, hours: 22, minutes: 59, seconds: 59, isUTC: true }
+                    }
+                }
+            },
+            {
+                component: 'vevent',
+                uid: {
+                    value: '7E018059-2165-4170-B32F-6936E88E61E5'
+                },
+                dtstart: {
+                    value: { year: 2019, month: 7, day: 19, hours: 12, minutes: 0, seconds: 0, isUTC: false },
+                    parameters: { tzid: 'America/New_York' }
+                },
+                dtend: {
+                    value: { year: 2019, month: 7, day: 19, hours: 13, minutes: 0, seconds: 0, isUTC: true }
+                },
+                rrule: {
+                    value: {
+                        freq: 'DAILY',
+                        until: { year: 2020, month: 1, day: 30, hours: 22, minutes: 59, seconds: 59, isUTC: true }
+                    }
+                }
+            }
+        ]);
+    });
+
+    it('should parse weekly rrule in vevent', () => {
+        const components = veventsRruleWeekly.map(parse);
 
         expect(components).toEqual([
             {
@@ -270,7 +384,14 @@ describe('calendar', () => {
                         until: { year: 2020, month: 1, day: 30, hours: 22, minutes: 59, seconds: 59, isUTC: true }
                     }
                 }
-            },
+            }
+        ]);
+    });
+
+    it('should parse monthly rrule in vevent', () => {
+        const components = veventsRruleMonthly.map(parse);
+
+        expect(components).toEqual([
             {
                 component: 'vevent',
                 uid: {
@@ -311,7 +432,14 @@ describe('calendar', () => {
                         until: undefined
                     }
                 }
-            },
+            }
+        ]);
+    });
+
+    it('should parse yearly rrule in vevent', () => {
+        const components = veventsRruleYearly.map(parse);
+
+        expect(components).toEqual([
             {
                 component: 'vevent',
                 uid: {
@@ -396,7 +524,7 @@ describe('calendar', () => {
     });
 
     it('should round trip rrule in vevent', () => {
-        const vevents = [...veventsRruleWeekly, ...veventsRruleMonthly, ...veventsRruleYearly];
+        const vevents = [...veventsRruleDaily, ...veventsRruleWeekly, ...veventsRruleMonthly, ...veventsRruleYearly];
         const results = vevents.map((vevent) => serialize(parse(vevent)));
         expect(results.map(trimAll)).toEqual(vevents.map(trimAll));
     });
