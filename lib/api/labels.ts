55be2ec2047262374fea07dd00e44efee5d70570
Richard <richard@protonmail.com>
2020-02-06 14:22:31 +0100

    Review

--- lib/api/labels.ts
+++ lib/api/labels.ts
@@ -2,31 +2,56 @@ import { LABEL_TYPE } from '../constants';
 
 const { MESSAGE_LABEL, MESSAGE_FOLDER, CONTACT_GROUP } = LABEL_TYPE;
 
-export const get = (Type) => ({
+export const get = (Type: number) => ({
     url: 'v4/labels',
     method: 'get',
     params: { Type }
 });
 
-export const order = ({ LabelIDs, ParentID, Type }) => ({
+export const order = ({ LabelIDs, ParentID, Type }: { LabelIDs: string[]; ParentID?: string; Type: number }) => ({
     method: 'put',
     url: 'v4/labels/order',
     data: { LabelIDs, ParentID, Type }
 });
 
-export const create = ({ Name, Color, Display, Type, Notify }) => ({
+export const create = ({
+    Name,
+    Color,
+    Type,
+    Notify,
+    ParentID,
+    Expanded
+}: {
+    Name: string;
+    Color: string;
+    Type: number;
+    ParentID?: string;
+    Notify?: number;
+    Expanded?: number;
+    Sticky?: number;
+}) => ({
     method: 'post',
     url: 'v4/labels',
-    data: { Name, Color, Display, Type, Notify }
+    data: { Name, Color, Type, Notify, ParentID, Expanded }
 });
 
-export const updateLabel = (labelID, { Name, Color, Notify, ParentID, Sticky, Expanded }) => ({
+export const updateLabel = (
+    labelID: string,
+    {
+        Name,
+        Color,
+        Notify,
+        ParentID,
+        Sticky,
+        Expanded
+    }: { Name: string; Color: string; Notify?: number; ParentID?: string; Sticky?: number; Expanded?: number }
+) => ({
     method: 'put',
     url: `v4/labels/${labelID}`,
     data: { Name, Color, Notify, ParentID, Sticky, Expanded }
 });
 
-export const deleteLabel = (labelID) => ({
+export const deleteLabel = (labelID: string) => ({
     method: 'delete',
     url: `v4/labels/${labelID}`
 });
