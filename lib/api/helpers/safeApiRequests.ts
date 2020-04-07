f2d1bf1f9797073317efa3b347c6def6f004475d
econdepe <econdepe@protonmail.com>
2020-04-07 19:27:30 +0200

    revamp safeApiRequest again

--- lib/api/helpers/safeApiRequests.ts
+++ lib/api/helpers/safeApiRequests.ts
@@ -1,47 +1,27 @@
-import { wait } from '../../helpers/promise';
-
 /**
  * Process multiple requests involving API calls safely to avoid getting jailed
  * @param interval      should be in milliseconds
  */
-export async function processApiRequestsSafe<T>(
-    requests: (() => Promise<T>)[],
-    apiCallsPerRequest = 1,
-    interval = 900 * 1000,
-    maxCallsPerInterval = 25000
-): Promise<T[]> {
-    const maxRequestsPerInterval = Math.floor(maxCallsPerInterval / apiCallsPerRequest);
-    const queue = requests.map((request, index) => ({ request, index }));
-    const results: T[] = [];
-    let totalResolved = 0;
-    let iterations = 0;
-
-    const process = async (): Promise<any> => {
-        const leftover = iterations * maxRequestsPerInterval - totalResolved;
-        const concurrentRequests = maxRequestsPerInterval - leftover;
-        if (concurrentRequests === 0) {
-            await wait(interval);
-            return process();
+export const processApiRequestsSafe = <T>(
+    promisesGenerators: (() => Promise<T>)[],
+    maxConcurrentPerInterval = 10,
+    interval = 1000
+): Promise<T[]> => {
+    const queue = [...promisesGenerators];
+    let results: Promise<T>[] = [];
+    let resolve: (data: Promise<T[]>) => void;
+    const run = () => {
+        const callbacks = queue.splice(0, maxConcurrentPerInterval);
+        const promises = callbacks.map((cb) => cb());
+        results = results.concat(promises);
+        if (queue.length) {
+            setTimeout(() => run(), interval);
+        } else {
+            resolve(Promise.all(results));
         }
-        const requests = queue.splice(0, concurrentRequests);
-        iterations += 1;
-        const promises = Promise.all(
-            requests.map(({ request, index }) => {
-                return request().then((result) => {
-                    totalResolved += 1;
-                    results[index] = result;
-                });
-            })
-        );
-        if (!queue.length) {
-            await promises;
-            return;
-        }
-        await wait(interval);
-        return process();
     };
-
-    await process();
-
-    return results;
-}
+    run();
+    return new Promise<T[]>((_resolve) => {
+        resolve = _resolve;
+    });
+};
