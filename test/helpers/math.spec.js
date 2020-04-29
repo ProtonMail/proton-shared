304f9d705878091ca5cc5eb785e4968ec34b7990
Mattias Svanström <mmso@mmso.se>
2020-04-29 09:51:05 +0000

    Apply suggestion to test/helpers/math.spec.js

--- test/helpers/math.spec.js
+++ test/helpers/math.spec.js
@@ -33,7 +33,7 @@ describe('math', () => {
     });
 
     describe('mod', () => {
-        it('should mod a number safely', () => {
+        it('should return a positive remainder', () => {
             expect(mod(-4, 3)).toEqual(2);
             expect(mod(-3, 3)).toEqual(0);
             expect(mod(-2, 3)).toEqual(1);
