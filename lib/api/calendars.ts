d312b96ada3377b01b0d8a0c650491a475a04bda
econdepe <econdepe@protonmail.com>
2020-06-15 11:04:33 +0200

    modify GetEventByArguments

--- lib/api/calendars.ts
+++ lib/api/calendars.ts
@@ -156,8 +156,10 @@ export const getEvent = (calendarID: string, eventID: string) => ({
     method: 'get',
 });
 
-interface GetEventByUIDArguments extends PaginationParams {
+interface GetEventByUIDArguments extends Partial<PaginationParams> {
     UID: string;
+    RecurrenceID?: number;
+    GroupID?: string;
 }
 export const getEventByUID = (params: GetEventByUIDArguments) => ({
     url: `${CALENDAR_V1}/events`,
