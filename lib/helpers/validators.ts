905c6fab6be4b851fcf25ef631403b2d4ebdc7c1
Jeremy Benaim <jeremybenaim@gmail.com>
2020-06-24 22:26:32 +0100

    Extend base64 images mime-types

--- lib/helpers/validators.ts
+++ lib/helpers/validators.ts
@@ -3,7 +3,7 @@ export const REGEX_URL = /((([A-Za-z]{3,9}:(?:\/\/)?)(?:[\-;:&=\+\$,\w]+@)?[A-Za
 export const REGEX_DOMAIN = /^[a-zA-Z0-9][a-zA-Z0-9-]{1,61}[a-zA-Z0-9](?:\.[a-zA-Z]{2,})+/;
 export const REGEX_HEX_COLOR = /^#([a-f0-9]{3,4}|[a-f0-9]{4}(?:[a-f0-9]{2}){1,2})\b$/i;
 export const REGEX_NUMBER = /^\d+$/;
-export const REGEX_BASE64_IMAGE = /^data:image\/(?:gif|png|jpeg|bmp|webp);base64/;
+export const REGEX_BASE64_IMAGE = /^data:image\/(?:gif|png|jpeg|bmp|webp|svg\+xml|apng|tiff);base64/;
 
 export const isEmpty = (value = '') => !value.length;
 export const maxLength = (value = '', limit = 0) => value.length <= limit;
