11adce6d13381b3709112c345bd8d22be36c44c8
mmso <mmso@mmso.se>
2020-07-23 10:26:46 +0200

    Themes in ts

--- lib/themes/helpers.ts
+++ lib/themes/helpers.ts
@@ -1,22 +1,23 @@
 import { PROTON_THEMES, CUSTOM_THEME, DEFAULT_THEME } from './themes';
 import { DARK_MODE_CLASS } from '../constants';
 
-const { protonThemeIdentifiers, protonThemes } = Object.values(PROTON_THEMES).reduce(
+const { protonThemeIdentifiers, protonThemes } = Object.values(PROTON_THEMES).reduce<{
+    protonThemeIdentifiers: string[];
+    protonThemes: { [key: string]: string };
+}>(
     (acc, { identifier, theme }) => {
         acc.protonThemeIdentifiers.push(identifier);
         acc.protonThemes[identifier] = theme;
         return acc;
     },
-    { protonThemeIdentifiers: [], protonThemes: Object.create(null) }
+    { protonThemeIdentifiers: [], protonThemes: {} }
 );
 const defaultThemeIdentifier = DEFAULT_THEME.identifier;
 
 /**
  * Given a theme, return identifier
- * @param {String} theme            CSS associated to a theme
- * @return {String}                 theme identifier
  */
-export const getThemeIdentifier = (theme) => {
+export const getThemeIdentifier = (theme: string) => {
     if (!theme) {
         return defaultThemeIdentifier;
     }
@@ -29,50 +30,40 @@ export const getThemeIdentifier = (theme) => {
 
 /**
  * Given a theme identifier, return true if it's the custom theme identifier, false otherwise
- * @param {String} themeIdentifier
- * @return {Boolean}
  */
-export const isCustomThemeIdentifier = (themeIdentifier) => {
+export const isCustomThemeIdentifier = (themeIdentifier: string) => {
     return themeIdentifier === CUSTOM_THEME.identifier;
 };
 
 /**
  * Given a theme, return true if it's a custom one, false otherwise
- * @param {String} theme
- * @return {Boolean}
  */
-export const isCustomTheme = (theme) => {
+export const isCustomTheme = (theme: string) => {
     return isCustomThemeIdentifier(getThemeIdentifier(theme));
 };
 
 /**
  * Given a theme, return true if it corresponds to dark mode, false otherwise
- * @param {String} theme
- * @return {Boolean}
  */
-export const isDarkTheme = (theme) => {
+export const isDarkTheme = (theme: string) => {
     return getThemeIdentifier(theme) === PROTON_THEMES.DARK.identifier;
 };
 
 /**
  * Given a theme identifier with commented code as '\/* something *\/', extract 'something'
- * @param {String} themeIdentifier      theme identifier with comment markers
- * @return {String}                     theme identifier without comment markers
  */
-export const stripThemeIdentifier = (themeIdentifier) => {
-    const regex = /\/\*(.*)\*\//;
-    if (regex.test(themeIdentifier)) {
-        return themeIdentifier.match(/\/\*(.*)\*\//)[1].trim();
+export const stripThemeIdentifier = (themeIdentifier: string) => {
+    const match = themeIdentifier.match(/\/\*(.*)\*\//);
+    if (match) {
+        return match[1].trim();
     }
     return themeIdentifier;
 };
 
 /**
  * Given a theme identifier, return theme
- * @param {String} themeIdentifier          theme identifier
- * @return {String}                         CSS associated with the theme
  */
-export const getTheme = (themeIdentifier) => {
+export const getTheme = (themeIdentifier: string) => {
     if (protonThemeIdentifiers.includes(themeIdentifier)) {
         return protonThemes[themeIdentifier];
     }
@@ -81,18 +72,13 @@ export const getTheme = (themeIdentifier) => {
 
 /**
  * Concat themes
- * @param {Array<String>} themes
- * @returns {String}
  */
-export const toStyle = (themes = []) => themes.join('\n');
+export const toStyle = (themes: string[] = []) => themes.join('\n');
 
 /**
  * Given two arguments, the second meant to be used in dark mode and the first in the other cases,
  * pick the appropiate one depending on whether the class 'isDarkMode' is in the body or not
- * @param {*} light
- * @param {*} dark
- * @return {*}
  */
-export const getLightOrDark = (light, dark) => {
+export const getLightOrDark = <A, B>(light: A, dark: B) => {
     return document.body.classList.contains(DARK_MODE_CLASS) ? dark : light;
 };
