41ea1557e14875a231c90bc09e99d521b07c5747
Ed Telezhnikov <edtelezhnikov@protonmail.com>
2020-07-29 17:09:42 +0300

    Reimplement autocomplete component
    CALWEB-691

--- lib/helpers/address.ts
+++ lib/helpers/address.ts
@@ -1,5 +1,6 @@
-import { ADDRESS_STATUS, ADDRESS_TYPE, RECEIVE_ADDRESS, SEND_ADDRESS } from '../constants';
-import { Address } from '../interfaces';
+import { ADDRESS_STATUS, ADDRESS_TYPE, RECEIVE_ADDRESS, SEND_ADDRESS, MAJOR_DOMAINS } from '../constants';
+import { Address, Recipient } from '../interfaces';
+import { ContactEmail } from '../interfaces/contacts';
 
 export const getActiveAddresses = (addresses: Address[]): Address[] => {
     return addresses.filter(({ Status, Receive, Send }) => {
@@ -17,4 +18,22 @@ export const hasAddresses = (addresses: Address[] | undefined): boolean => {
 
 export const getHasOnlyExternalAddresses = (addresses: Address[]) => {
     return addresses.every(({ Type }) => Type === ADDRESS_TYPE.TYPE_EXTERNAL);
+};
+
+export const contactToRecipient = (contact: Partial<ContactEmail> = {}, groupPath?: string): Partial<Recipient> => ({
+    Name: contact.Name,
+    Address: contact.Email,
+    ContactID: contact.ContactID,
+    Group: groupPath,
+});
+
+export const majorDomainsMatcher = (inputValue: string) => {
+    const [localPart, domainPart] = inputValue.split('@');
+    if (!localPart || typeof domainPart !== 'string') {
+        return [];
+    }
+    return MAJOR_DOMAINS.map((domain) => {
+        const email = `${localPart}@${domain}`;
+        return { Address: email, Name: email } as Recipient;
+    });
 };
