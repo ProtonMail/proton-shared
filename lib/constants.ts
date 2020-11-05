15d0ea94442d49e4482b119fd7b2e6fa20e846e0
Jeremy Benaim <jeremybenaim@gmail.com>
2020-11-05 11:11:03 +0100

    Sanity checkpoint

--- lib/constants.ts
+++ lib/constants.ts
@@ -773,11 +773,3 @@ export const FORKABLE_APPS = new Set(
 );
 
 export const AES256 = 'aes256';
-
-export const FOCUS_AREAS = {
-    PROTONMAIL: {
-        SIDEBAR: 'sidebar',
-        ELEMENT_LIST: 'element-list',
-        ELEMENT_DETAILS: 'element-details',
-    },
-};
