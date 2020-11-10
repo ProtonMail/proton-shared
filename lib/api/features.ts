5e48e16fea3f7f7f8c2392d16fdae10561339d56
Richard <richard@protonmail.com>
2020-11-10 16:19:11 +0100

    Review routes

--- lib/api/features.ts
+++ lib/api/features.ts
@@ -1,12 +1,10 @@
-import { Feature as FeatureModel } from '../interfaces/Feature';
-
 export const getFeature = (featureCode: string) => ({
     url: `features/${featureCode}`,
     method: 'get',
 });
 
-export const updateFeature = (Feature: FeatureModel) => ({
-    url: `features/${Feature.Code}`,
+export const updateFeatureValue = (featureCode: string, Value: any) => ({
+    url: `features/${featureCode}/value`,
     method: 'put',
-    data: { Feature },
+    data: { Value },
 });
