7f6880c607eea354345b3cb4dbda50c50da7555b
Jeremy Benaim <jeremybenaim@gmail.com>
2020-11-19 14:35:17 +0100

    New shortcuts modal

--- lib/shortcuts/constants.ts
+++ lib/shortcuts/constants.ts
@@ -11,51 +11,19 @@ export const getShortcutsForApp = (app: APP_NAMES) => {
         case APPS.PROTONMAIL_SETTINGS:
             return [
                 {
-                    name: c('Shortcut section name').t`Generic actions`,
+                    name: c('Shortcut section name').t`Basic navigation`,
+                    alwaysActive: true,
                     shortcuts: [
                         {
-                            name: c('Shortcut name').t`New message`,
-                            keys: 'N',
+                            name: c('Shortcut name').t`Move up`,
+                            keys: '↑',
                         },
-                        {
-                            name: c('Shortcut name').t`Apply / open`,
-                            keys: 'Enter',
-                        },
-                        // {
-                        //     name: c('Shortcut name').t`Cancel / close`,
-                        //     keys: 'Escape',
-                        // },
-                        {
-                            name: c('Shortcut name').t`Select / unselect`,
-                            keys: 'Space or X',
-                        },
-                        {
-                            name: c('Shortcut name').t`Select / unselect all`,
-                            keys: 'Shift + X',
-                        },
-                        {
-                            name: c('Shortcut name').t`Search`,
-                            keys: '/',
-                        },
-                        {
-                            name: c('Shortcut name').t`Undo`,
-                            keys: `${META_KEY} + Z`,
-                        },
-                    ],
-                },
-                {
-                    name: c('Shortcut section name').t`Content navigation`,
-                    shortcuts: [
                         {
                             name: c('Shortcut name').t`Jump to first`,
                             keys: `${META_KEY} + ↑`,
                         },
                         {
-                            name: c('Shortcut name').t`Previous`,
-                            keys: '↑',
-                        },
-                        {
-                            name: c('Shortcut name').t`Next`,
+                            name: c('Shortcut name').t`Move down`,
                             keys: '↓',
                         },
                         {
@@ -73,77 +41,24 @@ export const getShortcutsForApp = (app: APP_NAMES) => {
                     ],
                 },
                 {
-                    name: c('Shortcut section name').t`Message actions`,
+                    name: c('Shortcut section name').t`Basic actions`,
+                    alwaysActive: true,
                     shortcuts: [
                         {
-                            name: c('Shortcut name').t`Reply`,
-                            keys: 'R',
+                            name: c('Shortcut name').t`Apply / open`,
+                            keys: 'Enter',
                         },
                         {
-                            name: c('Shortcut name').t`Reply all`,
-                            keys: 'Shift + R',
+                            name: c('Shortcut name').t`Cancel / close`,
+                            keys: 'Escape',
                         },
                         {
-                            name: c('Shortcut name').t`Forward`,
-                            keys: 'Shift + F',
-                        },
-                        {
-                            name: c('Shortcut name').t`Load remote content`,
-                            keys: 'Shift + C',
-                        },
-                        {
-                            name: c('Shortcut name').t`Load embedded images`,
-                            keys: 'Shift + E',
-                        },
-                        {
-                            name: c('Shortcut name').t`Show original message`,
-                            keys: 'O',
-                        },
-                    ],
-                },
-                {
-                    name: c('Shortcut section name').t`Composer actions`,
-                    shortcuts: [
-                        {
-                            name: c('Shortcut name').t`Close draft`,
-                            keys: `${META_KEY} + W`,
-                        },
-                        {
-                            name: c('Shortcut name').t`Minimize composer`,
-                            keys: `${META_KEY} + M`,
-                        },
-                        {
-                            name: c('Shortcut name').t`Insert file`,
-                            keys: `${META_KEY} + I`,
-                        },
-                        {
-                            name: c('Shortcut name').t`Save draft`,
-                            keys: `${META_KEY} + S`,
-                        },
-                        {
-                            name: c('Shortcut name').t`Send email`,
-                            keys: `${META_KEY} + Enter`,
-                        },
-                        {
-                            name: c('Shortcut name').t`Add expiration time`,
-                            keys: `${META_KEY} + Shift + X`,
-                        },
-                        {
-                            name: c('Shortcut name').t`Add encryption`,
-                            keys: `${META_KEY} + Shift + E`,
-                        },
-                        {
-                            name: c('Shortcut name').t`Delete draft`,
-                            keys: `${META_KEY} + Backspace`,
-                        },
-                    ],
-                },
-                {
-                    name: c('Shortcut section name').t`Help actions`,
-                    shortcuts: [
-                        {
-                            name: c('Shortcut name').t`Open shortcuts modal`,
+                            name: c('Shortcut name').t`Open this modal`,
                             keys: '?',
+                        },
+                        {
+                            name: c('Shortcut name').t`Search`,
+                            keys: '/',
                         },
                         // {
                         //     name: c('Shortcut name').t`Launch command line`,
@@ -152,7 +67,7 @@ export const getShortcutsForApp = (app: APP_NAMES) => {
                     ],
                 },
                 {
-                    name: c('Shortcut section name').t`Folder navigation`,
+                    name: c('Shortcut section name').t`Folder shortcuts`,
                     shortcuts: [
                         {
                             name: c('Shortcut name').t`Go to Inbox`,
@@ -189,8 +104,78 @@ export const getShortcutsForApp = (app: APP_NAMES) => {
                     ],
                 },
                 {
-                    name: c('Shortcut section name').t`List actions`,
+                    name: c('Shortcut section name').t`Composer shortcuts`,
                     shortcuts: [
+                        {
+                            name: c('Shortcut name').t`Close draft`,
+                            keys: `${META_KEY} + W`,
+                        },
+                        {
+                            name: c('Shortcut name').t`Minimize composer`,
+                            keys: `${META_KEY} + M`,
+                        },
+                        {
+                            name: c('Shortcut name').t`Insert file`,
+                            keys: `${META_KEY} + I`,
+                        },
+                        {
+                            name: c('Shortcut name').t`Save draft`,
+                            keys: `${META_KEY} + S`,
+                        },
+                        {
+                            name: c('Shortcut name').t`Send email`,
+                            keys: `${META_KEY} + Enter`,
+                        },
+                        {
+                            name: c('Shortcut name').t`Add expiration time`,
+                            keys: `${META_KEY} + Shift + X`,
+                        },
+                        {
+                            name: c('Shortcut name').t`Add encryption`,
+                            keys: `${META_KEY} + Shift + E`,
+                        },
+                        {
+                            name: c('Shortcut name').t`Delete draft`,
+                            keys: `${META_KEY} + Backspace`,
+                        },
+                        // {
+                        //     name: c('Shortcut name').t`Undo send`,
+                        //     keys: `${META_KEY} + Z`,
+                        // },
+                    ],
+                },
+                {
+                    name: c('Shortcut section name').t`List shortcuts`,
+                    shortcuts: [
+                        {
+                            name: c('Shortcut name').t`Show unread emails`,
+                            keys: 'Shift + U',
+                        },
+                        {
+                            name: c('Shortcut name').t`Show read emails`,
+                            keys: 'Shift + R',
+                        },
+                        {
+                            name: c('Shortcut name').t`Show all emails`,
+                            keys: 'Shift + A',
+                        },
+                        {
+                            name: c('Shortcut name').t`Select / unselect`,
+                            keys: 'Space or X',
+                        },
+                        {
+                            name: c('Shortcut name').t`Select / unselect all`,
+                            keys: 'Shift + X',
+                        },
+                    ],
+                },
+                {
+                    name: c('Shortcut section name').t`Action shortcuts`,
+                    shortcuts: [
+                        {
+                            name: c('Shortcut name').t`New message`,
+                            keys: 'N',
+                        },
                         {
                             name: c('Shortcut name').t`Star`,
                             keys: '*',
@@ -235,22 +220,38 @@ export const getShortcutsForApp = (app: APP_NAMES) => {
                             name: c('Shortcut name').t`Empty folder`,
                             keys: `${META_KEY} + Shift + Backspace`,
                         },
+                        // {
+                        //     name: c('Shortcut name').t`Undo action`,
+                        //     keys: `${META_KEY} + Z`,
+                        // },
                     ],
                 },
                 {
-                    name: c('Shortcut section name').t`Sorting actions`,
+                    name: c('Shortcut section name').t`Message shortcuts`,
                     shortcuts: [
                         {
-                            name: c('Shortcut name').t`Show unread emails`,
-                            keys: 'Shift + U',
+                            name: c('Shortcut name').t`Reply`,
+                            keys: 'R',
                         },
                         {
-                            name: c('Shortcut name').t`Show read emails`,
+                            name: c('Shortcut name').t`Reply all`,
                             keys: 'Shift + R',
                         },
                         {
-                            name: c('Shortcut name').t`Show all emails`,
-                            keys: 'Shift + A',
+                            name: c('Shortcut name').t`Forward`,
+                            keys: 'Shift + F',
+                        },
+                        {
+                            name: c('Shortcut name').t`Load remote content`,
+                            keys: 'Shift + C',
+                        },
+                        {
+                            name: c('Shortcut name').t`Load embedded images`,
+                            keys: 'Shift + E',
+                        },
+                        {
+                            name: c('Shortcut name').t`Show original message`,
+                            keys: 'O',
                         },
                     ],
                 },
