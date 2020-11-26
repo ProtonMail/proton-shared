9ddf4e771416001b3a4011b01f635d1dec2972f4
Jeremy Benaim <jeremybenaim@gmail.com>
2020-11-26 17:53:27 +0100

    Remove shift + r to show read emails

--- lib/shortcuts/constants.ts
+++ lib/shortcuts/constants.ts
@@ -151,10 +151,6 @@ export const getShortcutsForApp = (app: APP_NAMES) => {
                             keys: 'Shift + U',
                         },
                         {
-                            name: c('Shortcut name').t`Show read emails`,
-                            keys: 'Shift + R',
-                        },
-                        {
                             name: c('Shortcut name').t`Show all emails`,
                             keys: 'Shift + A',
                         },
