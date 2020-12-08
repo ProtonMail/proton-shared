d9c8b1dfcc1e9b9583744a677c5345f8008bc237
econdepe <econdepe@protonmail.com>
2020-12-08 22:55:16 +0100

    Revamp getIsRruleSubset and add unit tests
    CALWEB-1840, CALWEB-1841, CALWEB-1843, CALWEB-1846

--- test/calendar/rrule/rruleSubset.spec.js
+++ test/calendar/rrule/rruleSubset.spec.js
@@ -14,6 +14,10 @@ describe('rrule subset', () => {
             parameters: { tzid: 'America/New_York' },
         },
     };
+    const dummyUntil = {
+        value: { year: 2021, month: 1, day: 10, hours: 23, minutes: 59, seconds: 59, isUTC: false },
+        parameters: { tzid: 'America/New_York' },
+    };
     [
         getTest(
             { ...dummyVevent, rrule: { value: { freq: FREQUENCY.DAILY, count: 10 } } },
@@ -21,8 +25,65 @@ describe('rrule subset', () => {
             true
         ),
         getTest(
+            { ...dummyVevent, rrule: { value: { freq: FREQUENCY.DAILY, interval: 2 } } },
+            { ...dummyVevent, rrule: { value: { freq: FREQUENCY.DAILY } } },
+            true
+        ),
+        getTest(
+            { ...dummyVevent, rrule: { value: { freq: FREQUENCY.DAILY, interval: 2, until: dummyUntil } } },
+            { ...dummyVevent, rrule: { value: { freq: FREQUENCY.DAILY, until: dummyUntil } } },
+            true
+        ),
+        getTest(
+            {
+                ...dummyVevent,
+                rrule: { value: { freq: FREQUENCY.WEEKLY, byday: ['MO', 'TU', 'WE', 'TH', 'FR', 'SA', 'SU'] } },
+            },
+            { ...dummyVevent, rrule: { value: { freq: FREQUENCY.DAILY } } },
+            true
+        ),
+        getTest(
+            { ...dummyVevent, rrule: { value: { freq: FREQUENCY.WEEKLY, byday: ['WE', 'FR', 'SU'], count: 3 } } },
+            { ...dummyVevent, rrule: { value: { freq: FREQUENCY.DAILY, interval: 2, count: 3 } } },
+            true
+        ),
+        getTest(
             { ...dummyVevent, rrule: { value: { freq: FREQUENCY.DAILY } } },
             { ...dummyVevent, rrule: { value: { freq: FREQUENCY.DAILY, count: 10 } } },
+            false
+        ),
+        getTest(
+            { ...dummyVevent, rrule: { value: { freq: FREQUENCY.DAILY, interval: 2 } } },
+            { ...dummyVevent, rrule: { value: { freq: FREQUENCY.DAILY, interval: 3 } } },
+            false
+        ),
+        getTest(
+            { ...dummyVevent, rrule: { value: { freq: FREQUENCY.MONTHLY, byday: 'WE', bysetpos: 1 } } },
+            { ...dummyVevent, rrule: { value: { freq: FREQUENCY.MONTHLY } } },
+            false
+        ),
+        getTest(
+            {
+                ...dummyVevent,
+                rrule: {
+                    value: {
+                        freq: FREQUENCY.WEEKLY,
+                        interval: 2,
+                        byday: ['MO', 'TU', 'WE', 'TH', 'FR', 'SA', 'SU'],
+                        wkst: 'SU',
+                    },
+                },
+            },
+            {
+                ...dummyVevent,
+                rrule: {
+                    value: {
+                        freq: FREQUENCY.WEEKLY,
+                        interval: 2,
+                        byday: ['MO', 'TU', 'WE', 'TH', 'FR', 'SA', 'SU'],
+                    },
+                },
+            },
             false
         ),
     ].forEach(({ a, b, result }, i) => {
