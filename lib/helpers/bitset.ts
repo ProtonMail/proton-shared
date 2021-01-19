6223dbd27fa486c7d4a3584522d22bc5173b8ace
matthiasmargot <margotmatthias@protonmail.com>
2021-01-19 18:06:24 +0100

    Make getBits smarter

--- lib/helpers/bitset.ts
+++ lib/helpers/bitset.ts
@@ -21,7 +21,9 @@ export const hasBit = (number = 0, mask: number): boolean => (number & mask) ===
 /**
  * Get all bits which are toggled on in the respective bitmap
  */
-export const getBits = (bitmap: number, size: number) => {
+export const getBits = (bitmap: number) => {
+    const size = Math.floor(Math.log2(bitmap)) + 1;
+
     const bits: number[] = [];
 
     for (let i = 0; i <= size; i++) {
