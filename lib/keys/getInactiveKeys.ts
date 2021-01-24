328c76c132ac48ca8a0df57f86470b4638f93691
mmso <mmso@mmso.se>
2021-01-24 13:43:39 +0100

    Naming

--- lib/keys/getInactiveKeys.ts
+++ lib/keys/getInactiveKeys.ts
@@ -2,8 +2,8 @@ import { getKeys } from 'pmcrypto';
 import { DecryptedKey, Key } from '../interfaces';
 
 export const getInactiveKeys = async (Keys: Key[], decryptedKeys: DecryptedKey[]) => {
-    const keyPairsMap = new Set<string>(decryptedKeys.map(({ ID }) => ID));
-    const inactiveKeys = Keys.filter(({ ID }) => !keyPairsMap.has(ID));
+    const decryptedKeysIDs = new Set<string>(decryptedKeys.map(({ ID }) => ID));
+    const inactiveKeys = Keys.filter(({ ID }) => !decryptedKeysIDs.has(ID));
     return Promise.all(
         inactiveKeys.map(async (Key) => {
             const [privateKey] = await getKeys(Key.PrivateKey).catch(() => []);
