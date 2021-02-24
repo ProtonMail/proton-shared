cd6cb46ab3bfb1b8f03fbd8c1604510a201b40cc
mmso <mmso@mmso.se>
2021-02-24 17:11:23 +0100

    Declare missing types

--- typings/index.d.ts
+++ typings/index.d.ts
@@ -6,4 +6,6 @@ declare module 'is-valid-domain';
 
 declare module 'mimemessage';
 
+type UnderlyingByteSource = undefined;
+type ReadableStreamBYOBReader = undefined;
 declare module '@mattiasbuelens/web-streams-adapter';
