913d02808a33ca506216ed2c3068ad0ba3cb2df2
Sergo Vainumäe <sergo.vainumae@protonmail.ch>
2021-03-05 07:46:24 +0100

    Fix, improve, refactor
    CALWEB-2036

--- lib/keys/calendarSharing.ts
+++ lib/keys/calendarSharing.ts
@@ -2,30 +2,40 @@ import getRandomValues from 'get-random-values';
 import {
     arrayToBinaryString,
     createMessage,
+    decodeBase64,
     decryptMessage,
     encryptMessage,
-    generateSessionKey,
     getMessage,
     OpenPGPKey,
 } from 'pmcrypto';
 import { ACCESS_LEVEL } from '../calendar/interface';
-import { AES256 } from '../constants';
 import { encodeBase64 } from '../helpers/base64';
 import { stringToUint8Array, uint8ArrayToString, uint8ArrayToPaddedBase64URLString } from '../helpers/encoding';
 import { getSHA256Base64String } from '../helpers/hash';
 import { Nullable } from '../interfaces/utils';
 
 export const generateRandomBits = (number: number) => getRandomValues(new Uint8Array(number / 8));
+export const generateRandomBytes = (number: number) => getRandomValues(new Uint8Array(number));
 
 export const keyCharAt = (key: string, i: number) => key.charCodeAt(Math.floor(i % key.length));
 
 const xor = (key: string, data: string) => {
-    let result = '';
+    const Uint8Key = stringToUint8Array(key);
+    const Uint8Data = stringToUint8Array(data);
 
-    for (let i = 0; i < data.length; i++) {
-        result += String.fromCharCode(keyCharAt(key, i) ^ data.charCodeAt(i));
+    const xored = new Uint8Array(Uint8Data.length);
+
+    for (let j = 0; j < Uint8Data.length; j++) {
+        xored[j] = +Uint8Key[j] ^ +Uint8Data[j];
     }
-    return result;
+
+    // Strip off padding
+    let unpaddedLength = Uint8Data.length;
+    while (unpaddedLength > 0 && xored[unpaddedLength - 1] === 0) {
+        unpaddedLength--;
+    }
+
+    return uint8ArrayToString(xored.slice(0, unpaddedLength));
 };
 
 export const decryptPurpose = async ({
@@ -69,8 +79,8 @@ export const generateEncryptedPassphrase = ({
     decryptedPassphrase: string;
 }) => encodeBase64(xor(uint8ArrayToString(passphraseKey), atob(decryptedPassphrase)));
 
-export const generateCacheKey = async () => uint8ArrayToPaddedBase64URLString(await generateSessionKey(AES256));
-export const generateCacheKeySalt = () => encodeBase64(arrayToBinaryString(generateRandomBits(64)));
+export const generateCacheKey = async () => uint8ArrayToPaddedBase64URLString(generateRandomBytes(16));
+export const generateCacheKeySalt = () => encodeBase64(arrayToBinaryString(generateRandomBytes(8)));
 
 export const getCacheKeyHash = async ({ cacheKey, cacheKeySalt }: { cacheKey: string; cacheKeySalt: string }) =>
     getSHA256Base64String(`${cacheKeySalt}${cacheKey}`);
@@ -109,7 +119,13 @@ export const getPassphraseKey = ({
 }: {
     encryptedPassphrase: Nullable<string>;
     calendarPassphrase: string;
-}) => (encryptedPassphrase ? stringToUint8Array(xor(calendarPassphrase, encryptedPassphrase)) : null);
+}) => {
+    if (!encryptedPassphrase) {
+        return null;
+    }
+
+    return stringToUint8Array(xor(atob(calendarPassphrase), decodeBase64(encryptedPassphrase)));
+};
 
 export const encodePassphraseKey = (passphraseKey: Uint8Array) => {
     return uint8ArrayToPaddedBase64URLString(passphraseKey);
