7b53088f8d8d44eb37588aa117ec4f4368c05ed3
Sergo Vainumäe <sergo.vainumae@protonmail.ch>
2021-04-01 15:57:32 +0200

    Extract linkify from mail
    CALWEB-2256

---
+++ lib/helpers/linkify.ts
@@ -0,0 +1,45 @@
+import LinkifyIt from 'linkify-it';
+
+/**
+ * @{link https://css-tricks.com/snippets/javascript/htmlentities-for-javascript/}
+ */
+const htmlEntities = (str = '') => {
+    return String(str).replace(/&/g, '&amp;').replace(/</g, '&lt;').replace(/>/g, '&gt;').replace(/"/g, '&quot;');
+};
+
+const linkifyInstance = new LinkifyIt();
+
+// Reference Angular/src/helpers/linkifyHelper.js
+
+/**
+ * Auto link content.
+ */
+export const linkify = (content = '') => {
+    const matches = linkifyInstance.match(content);
+
+    if (!matches) {
+        return htmlEntities(content);
+    }
+
+    let last = 0;
+    const result = matches.reduce<string[]>((result, match) => {
+        if (last < match.index) {
+            result.push(htmlEntities(content.slice(last, match.index)));
+        }
+        result.push('<a target="_blank" rel="noreferrer nofollow noopener" href="');
+        result.push(htmlEntities(match.url));
+        result.push('">');
+        result.push(htmlEntities(match.text));
+        result.push('</a>');
+
+        last = match.lastIndex;
+
+        return result;
+    }, []);
+
+    if (last < content.length) {
+        result.push(htmlEntities(content.slice(last)));
+    }
+
+    return result.join('');
+};
