d4bc697f4d34e6a9c42d7526d3e45e0947b5773d
Sergo Vainumäe <sergo.vainumae@protonmail.ch>
2021-05-19 12:53:45 +0200

    Fix export

--- lib/calendar/attendees.ts
+++ lib/calendar/attendees.ts
@@ -242,12 +242,10 @@ const { REQUIRED } = ICAL_ATTENDEE_ROLE;
 const { TRUE } = ICAL_ATTENDEE_RSVP;
 const { NEEDS_ACTION } = ICAL_ATTENDEE_STATUS;
 
-const emailToAttendee = (email: string): AttendeeModel => ({
+export const emailToAttendee = (email: string): AttendeeModel => ({
     email,
     cn: email,
     role: REQUIRED,
     partstat: NEEDS_ACTION,
     rsvp: TRUE,
 });
-
-export default emailToAttendee;
