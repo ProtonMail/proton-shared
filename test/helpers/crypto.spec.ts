a243a44e1674ab4ba875866bc795df934b8fe060
econdepe <econdepe@protonmail.com>
2021-05-25 17:31:04 +0200

    Do not strip trailing zeros when using xorEncryptDecrypt
    CALWEB-2512

--- test/helpers/crypto.spec.ts
+++ test/helpers/crypto.spec.ts
@@ -9,5 +9,13 @@ describe('Crypto helpers', () => {
 
             expect(xorEncryptDecrypt({ key, data: xored })).toEqual(data);
         });
+
+        it('does not strip trailing zeros when encrypting or decrypting', () => {
+            const key = 'dogz';
+            const data = 'cat\u0000';
+            const xored = xorEncryptDecrypt({ key, data });
+
+            expect(xorEncryptDecrypt({ key, data: xored })).toEqual(data);
+        });
     });
 });
