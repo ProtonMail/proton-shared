0fa5211ca41a04408c285a464d8f26bc547b4f74
Matthias Margot <margotmatthias@protonmail.com>
2021-06-02 16:33:46 +0200

    Scrap the "app" parameter

--- lib/api/core/load.ts
+++ lib/api/core/load.ts
@@ -1,5 +1,5 @@
-export const load = (app: string, page: string) => ({
+export const load = (page: string) => ({
     method: 'post',
     url: 'core/v4/load',
-    params: { app, page },
+    params: { page },
 });
